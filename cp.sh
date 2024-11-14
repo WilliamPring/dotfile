6915e8ffc55b72bdf78b9f1838828bddba5dca5f
William Pring <winvmlliam.pring@pm.me>
2024-11-14 00:44:17 -0500

    feat: cp scripts

--- cp.sh
+++ cp.sh
@@ -1,13 +1,28 @@
-cp ~/.config/i3/config .config/i3
-cp ~/.config/i3status/config .config/i3status/config
-cp ~/.config/ranger/rc.conf .config/ranger
-cp ~/.vimrc .
-cp ~/.Xresources .
-cp ~/.zshrc .
-cp ~/.tmux.conf .
-cp ~/.config/starship.toml .config/
-cp ~/.config/newsboat .config/newsboat
-cp ~/.config/rofi/conifg.rasi .config/rofi/conifg.rasi
-cp ~/.config/keyboard .config/keyboard
-cp ~/.config/antigenrc .config/antigenrc
-cp ~/.config/audio-mapper.carxp .config/audio-mapper.carxp 
+#!/bin/bash
+
+SOURCE_DIR="$HOME/.config"  # .config directory as source
+DEST_DIR="$HOME/repos/dotfile/.config"  # Destination directory
+
+# Directories to be copied
+ITEMS=("keyboard" "newsboat" "rofi" "starship.toml" "i3" "i3status" "i3status-rust" "ranger" "antigenrc" "antigenrc.zwc" "audio-mapper.carxp" "will-linux.qpwgraph")
+
+# Loop through each item and copy it to the destination
+for ITEM in "${ITEMS[@]}"; do
+    SOURCE_PATH="$SOURCE_DIR/$ITEM"
+    DEST_PATH="$DEST_DIR/$ITEM"
+    
+    if [ -d "$SOURCE_PATH" ]; then
+        # If it's a directory, copy recursively
+        echo "Copying directory $ITEM..."
+        cp -r "$SOURCE_PATH" "$DEST_PATH"
+    elif [ -f "$SOURCE_PATH" ]; then
+        # If it's a file, copy normally
+        echo "Copying file $ITEM..."
+        cp "$SOURCE_PATH" "$DEST_PATH"
+    else
+        # If it's neither a file nor directory, print a message
+        echo "$ITEM does not exist in the source directory."
+    fi
+done
+
+echo "Sync completed."
