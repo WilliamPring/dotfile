f404a2c47b597840eb93124f3f375be75ea1b7c1
William Pring <winvmlliam.pring@pm.me>
2024-11-14 01:14:10 -0500

    sync file and home

--- cp.sh
+++ cp.sh
@@ -1,28 +1,58 @@
 #!/bin/bash
 
-SOURCE_DIR="$HOME/.config"  # .config directory as source
-DEST_DIR="$HOME/repos/dotfile/.config"  # Destination directory
+# Define source and destination for dotfiles
+SOURCE_CONFIG_DIR="$HOME/.config"  # .config directory as source
+SOURCE_HOME_DIR="$HOME"  # Home directory as source
+DEST_CONFIG_DIR="$HOME/repos/dotfile/.config"  # Destination directory for .config files
+DEST_HOME_DIR="$HOME/repos/dotfile/.config-home"  # Destination directory for home directory files
 
-# Directories to be copied
-ITEMS=("keyboard" "newsboat" "rofi" "starship.toml" "i3" "i3status" "i3status-rust" "ranger" "antigenrc" "antigenrc.zwc" "audio-mapper.carxp" "will-linux.qpwgraph")
 
-# Loop through each item and copy it to the destination
-for ITEM in "${ITEMS[@]}"; do
-    SOURCE_PATH="$SOURCE_DIR/$ITEM"
-    DEST_PATH="$DEST_DIR/$ITEM"
-    
-    if [ -d "$SOURCE_PATH" ]; then
-        # If it's a directory, copy recursively
-        echo "Copying directory $ITEM..."
-        cp -r "$SOURCE_PATH" "$DEST_PATH"
-    elif [ -f "$SOURCE_PATH" ]; then
-        # If it's a file, copy normally
-        echo "Copying file $ITEM..."
-        cp "$SOURCE_PATH" "$DEST_PATH"
-    else
-        # If it's neither a file nor directory, print a message
-        echo "$ITEM does not exist in the source directory."
-    fi
-done
+# List of directories to be copied from ~/.config
+CONFIG_ITEMS=("keyboard" "newsboat" "rofi" "starship.toml" "i3" "i3status" "ranger" "audio-mapper.carxp")
+
+# List of files to be copied from ~ (home directory)
+HOME_FILES=(".zshrc" ".vimrc" ".Xauthority" ".xinitrc" ".Xresources")
+
+
+# Function to sync dotfiles from ~/.config to the repo
+SYNC_CONFIG_DOTFILES() {
+    echo "Syncing dotfiles from .config..."
+    for ITEM in "${CONFIG_ITEMS[@]}"; do
+        SOURCE_PATH="$SOURCE_CONFIG_DIR/$ITEM"
+        DEST_PATH="$DEST_CONFIG_DIR/$ITEM"
+        
+        if [ -d "$SOURCE_PATH" ]; then
+            # If it's a directory, copy recursively
+            echo "Copying directory $ITEM..."
+            cp -r "$SOURCE_PATH" "$DEST_PATH"
+        elif [ -f "$SOURCE_PATH" ]; then
+            # If it's a file, copy normally
+            echo "Copying file $ITEM..."
+            cp "$SOURCE_PATH" "$DEST_PATH"
+        else
+            echo "$ITEM does not exist in the source directory."
+        fi
+    done
+}
+
+# Function to sync dotfiles from ~ (home directory) to the repo
+SYNC_HOME_DOTFILES() {
+    echo "Syncing dotfiles from home directory..."
+    for ITEM in "${HOME_FILES[@]}"; do
+        SOURCE_PATH="$SOURCE_HOME_DIR/$ITEM"
+        DEST_PATH="$DEST_HOME_DIR/$ITEM"
+        
+        if [ -f "$SOURCE_PATH" ]; then
+            # If it's a file, copy normally
+            echo "Copying file $ITEM..."
+            cp "$SOURCE_PATH" "$DEST_PATH"
+        else
+            echo "$ITEM does not exist in the source directory."
+        fi
+    done
+}
+
+SYNC_CONFIG_DOTFILES
+SYNC_HOME_DOTFILES
 
 echo "Sync completed."
